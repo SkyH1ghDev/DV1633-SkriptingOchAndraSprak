e1d389c0e98dd57b2a916ad0f9dcfb17de68a38d
SkyH1ghDev <95106202+SkyH1ghDev@users.noreply.github.com>
2025-04-07 22:18:26 +0200

    Submoduled LuaJIT

--- External/premake5.lua
+++ External/premake5.lua
@@ -49,3 +49,29 @@ project "RayLib"
             "cmake -S " .. libDirectory .. " -B %{prj.objdir} -DCMAKE_INSTALL_PREFIX=%{prj.targetdir}",
             "cmake --build %{prj.objdir} --config %{cfg.buildcfg} --target install",
         }
+
+project "RayLib"
+
+    kind "StaticLib"
+    location(rootPath .. "/Generated/Projects")
+
+    targetdir(targetBuildPath .. "/External")
+    objdir(objBuildPath .. "/%{prj.name}")
+
+    libDirectory = "\"" .. path.getdirectory(_SCRIPT) .. "\"" .. "/%{prj.name}"
+
+    filter "system:windows"
+        kind "Utility"
+        prebuildcommands{
+            "{MKDIR} %{prj.objdir}",
+            "cmake -S " .. libDirectory .. " -B %{prj.objdir} -DCMAKE_INSTALL_PREFIX=%{prj.targetdir} -DCMAKE_INSTALL_INCLUDEDIR=include/raylib -DCMAKE_MSVC_RUNTIME_LIBRARY='MultiThreadedDebug'",
+            "cmake --build %{prj.objdir} --config %{cfg.buildcfg} --target install",
+        }
+
+    --[[filter "system:linux"
+        kind "Makefile"
+        buildcommands{
+            "{MKDIR} %{prj.objdir}",
+            "cmake -S " .. libDirectory .. " -B %{prj.objdir} -DCMAKE_INSTALL_PREFIX=%{prj.targetdir}",
+            "cmake --build %{prj.objdir} --config %{cfg.buildcfg} --target install",
+        }]]--
